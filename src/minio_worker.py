cbd58b9808d61a6625b6d9555a1f18eaf6f5f050
Dan1aR <andan.com@mail.ru>
2023-11-22 00:43:16 +0300

    zone Moscow

--- src/minio_worker.py
+++ src/minio_worker.py
@@ -5,6 +5,7 @@ from typing import Dict
 from datetime import datetime
 
 from minio import Minio
+import zoneinfo
 
 from src.utils.logging_utils import get_logger
 
@@ -12,6 +13,7 @@ MINIO_ENDPOINT = os.environ["S3_ENDPOINT"]
 ACCESS_KEY = os.environ["S3_ACCESS_KEY_ID"]
 SECRET_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
 S3_BUCKET = os.environ["S3_BUCKET"]
+ZONE = zoneinfo.ZoneInfo("Europe/Moscow")
 
 
 def minio_worker(
@@ -43,10 +45,11 @@ def minio_worker(
 
         try:
             # Generating object name
+            now = datetime.now(ZONE)
             object_name = os.path.basename(file_path)
             object_name = os.path.join(
-                datetime.now().date().strftime("%Y-%m-%d"),
-                "_".join([datetime.now().strftime("%H"), object_name]),
+                now.date().strftime("%Y-%m-%d"),
+                "_".join([now.strftime("%H"), object_name]),
             )
             with open(file_path, "rb") as fin:
                 file_bytes = fin.read()
